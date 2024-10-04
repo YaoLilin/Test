81227adb9af1010fc1ac5ac89b123b48f40334c9
yaolilin <yaolilin@outlook.com>
2024-10-04 19:02:21 +0800

    dev3

--- src/java/Application.java
+++ src/java/Application.java
@@ -5,6 +5,7 @@
  **/
 public class Application {
     public static void main(String[] args) {
+        System.out.println("开发3");
         System.out.println("开发2");
         System.out.println("hello world");
     }
