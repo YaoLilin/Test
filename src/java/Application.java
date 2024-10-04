086b39a59e3dd34a68c6c3435838f0a6c42fbe43
yaolilin <yaolilin@outlook.com>
2024-10-04 15:37:02 +0800

    创建java项目

---
+++ src/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * @author yaolilin
+ * @desc todo
+ * @date 2024/10/4
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
