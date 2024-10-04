ced85832083b92308bd68f1789f84c2a1fd4dd45
yaolilin <yaolilin@outlook.com>
2024-10-04 18:53:52 +0800

    main1

--- src/java/Application.java
+++ src/java/Application.java
@@ -7,5 +7,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("开发1");
         System.out.println("hello world");
+        System.out.println("正式功能");
     }
 }
